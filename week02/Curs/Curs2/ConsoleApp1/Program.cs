a3fb3d33369239b9e77b94d0b9d298d56311afbb
gamatei <alex@matei.ltd>
2020-07-17 13:51:57 +0300

    curs

---
+++ week02/Curs/Curs2/ConsoleApp1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listGuess1 = new List<int>();
+            List<int> listGuess2 = new List<int>();
+            int x = new Random().Next(1, 10);
+            int inserted = int.Parse(Console.ReadLine());
+            int i = 0;
+
+            while (inserted!=x)
+            {
+                if (inserted < 5)
+                {
+                    listGuess1.Add(inserted);
+                }
+                else
+                {
+                    listGuess2.Add(inserted);
+                }
+                i++;
+            }
+            Console.WriteLine($"You made {listGuess1.Count+listGuess2.Count} guesses");
+        }
+
+
+        static void Curs()
+        {
+            List<int> myList = new List<int>();
+            myList.Add(4);
+            myList.Add(7);
+            myList.Add(8);
+
+            List<int> myList1 = new List<int>();
+            myList1.AddRange(myList);
+            myList1.Add(9);
+            bool result = myList1.Contains(4);
+
+            LinkedList<int> myLinkedList = new LinkedList<int>();
+            myLinkedList.AddFirst(1);
+            myLinkedList.AddFirst(2);
+        }
+
+        static void Listnumber()
+        {
+            int startNumber = 1;
+            int endNumber = 10;
+            List<int> myList = new List<int>();
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i != 4 && i != 7)
+                {
+                    myList.Add(i);
+                }
+
+            }
+            string listToPrint = "";
+            foreach (int x in myList)
+            {
+                listToPrint += $" {x} ";
+            }
+            Console.WriteLine(listToPrint);
+        }
+
+
+        static void Fibo()
+        {
+            int start = 0;
+            int next = 1;
+            int sum;
+
+            List<int> fibo = new List<int>() { start, next };
+
+            do
+            {
+                sum = start + next;
+                fibo.Add(sum);
+                start = next;
+                next = sum;
+            } while (start + next <= 50);
+
+            string listToPrint = "";
+
+            foreach (int x in fibo)
+            {
+                listToPrint += $"{x} ";
+            }
+
+            Console.WriteLine(listToPrint);
+
+        }
+
+        static void Remove()
+        {
+            List<int> noList = new List<int>() { 12, 3, 5, 7, 3, 16 };
+            List<int> newList = new List<int>();
+
+            bool threefound = false;
+            for (int i = 0; i <= noList.Count - 1; i++)
+            {
+                if (noList[i] == 3 && !threefound)
+                {
+                    threefound = true;
+                    continue;
+                }
+
+                newList.Add(noList[i]);
+            }
+
+            string listToPrint = "";
+            foreach (int x in newList)
+            {
+                listToPrint += $"{x} ";
+            }
+
+            Console.WriteLine(listToPrint);
+
+        }
+
+        static void Minmax1()
+        {
+            List<int> myList = new List<int> { 16, 22, 5, 7, 36, 0 };
+            int max = myList[0];
+            int min = myList[0];
+            for (int i = 1; i < myList.Count; i++)
+            {
+                if (max < myList[i])
+                {
+                    max = myList[i];
+                }
+                if (min > myList[i])
+                {
+                    min = myList[i];
+                }
+            }
+            Console.WriteLine($"Min : {min} , Max: {max}");
+
+
+     
+
+        }
+
+        static void Minmax2()
+        {
+            List<int> myList = new List<int> { 16, 22, 5, 7, 36, 0 };
+            int max = myList.Max();
+            int min = myList.Min();
+
+            Console.WriteLine($"Min : {min} , Max: {max}");
+
+
+
+        }
+    }
+}
---
+++ week02/Curs/curs/ConsoleApp1/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        /// <summary>
+        /// reverse array
+        /// </summary>
+        static void Ex1()
+        {
+            var array = new int[5] { 1, 32, 3, 4, 5 };
+            var newArray = new int[5];
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                int newPoz = array.Length - 1 - i;
+                newArray[newPoz] = array[i];
+            }
+            array[array.Length - 1] = 0;
+            foreach (int value in newArray)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+
+        /// <summary>
+        /// even no. from 1 to 10 in array
+        /// </summary>
+        static void Ex2()
+        {
+            int startNumber = 1;
+            int endNumber = 10;
+            int counter = 0;
+
+            var array1 = new int[7];
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    array1[counter] = i;
+                    counter++;
+                }
+            }
+            foreach (int value in array1)
+            {
+                Console.WriteLine(value);
+            }
+        }
+        /// <summary>
+        /// Print no without 1 or 10
+        /// </summary>
+        static void Ex3()
+        {
+            var array = new int[] { 13, 1, 15, 7, 19 };
+            foreach (int v in array)
+            {
+                if (v == 1 || v == 10)
+                {
+                    continue;
+                }
+                Console.WriteLine(v);
+            }
+        }
+
+        /// <summary>
+        /// average for an array
+        /// </summary>
+        static void Ex4()
+        {
+            var array = new int[5];
+            Console.WriteLine("please provide 5 numbers separated by comma");
+            string input1 = Console.ReadLine();
+            string[] separatedNumbers = input1.Split(',');
+            for (var input = 0; input <= 4; input++)
+            {
+                array[input] = int.Parse(separatedNumbers[input]);
+            }
+            var sum = 0;
+            foreach (var nr in array)
+            {
+                sum = sum + nr;
+            }
+            double av = (double)sum / array.Length;
+            Console.WriteLine($"the average is: {av}");
+        }
+
+        /// <summary>
+        /// 
+        ///max & min of array
+        /// </summary>
+        /// <param name="args"></param>
+        static void Ex5(string[] args)
+        {
+            int[] array = new int[] { 20, 11, 45, 2, 62, 1 };
+            int max = -1,
+                min = int.MaxValue;
+            foreach (int val in array)
+            {
+                if (val < min)
+                {
+                    min = val;
+
+                }
+                if (val > max)
+                {
+                    max = val;
+                }
+
+            }
+            Console.WriteLine("max is: " + max);
+            Console.WriteLine("min is: " + min);
+        }
+
+
+        /// <summary>
+        /// 
+        ///remove element from array
+        /// </summary>
+        static void Ex6()
+        {
+            int[] array = new int[] { 20, 11, 45, 2, 62, 1 };
+            int pos = 2;
+            for (int i = pos + 1; i <= array.Length - 1; i++)
+            {
+                array[i - 1] = array[i];
+            }
+            array[array.Length - 1] = 0;
+
+            foreach (int val in array)
+            {
+                Console.WriteLine(val);
+
+            }
+        }
+
+        /// <summary>
+        /// find no. in array
+        /// </summary>
+        static void Ex7()
+        {
+            int indexFound = -1;
+            int[] array = new int[] { 12, 7, 67, 69, 5 };
+            int value = 69;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    indexFound = i;
+                    break;
+                }
+            }
+            Console.WriteLine($"Index found on position: {indexFound}");
+        }
+
+        /// <summary>
+        /// copy elements of array in another array
+        /// </summary>
+        /// <param name="args"></param>
+        static void Ex8()
+        {
+            int[] array = new int[] { 12, 7, 67, 69, 5 };
+            int[] array1 = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array1[i] = array[i];
+            }
+            foreach (int v in array1)
+            {
+                Console.WriteLine(v);
+            }
+        }
+
+        /// <summary>
+        /// find common elements in arrays
+        /// </summary>
+        static void Ex9()
+        {
+            int[] array1 = new int[] { 123, 5, 423, 6, 42, 444, 2, 3 };
+            int[] array2 = new int[] { 5, 23, 7878, 333, 42, 1, 0, 444 };
+            foreach (int valueFromFirstArray in array1)
+            {
+                foreach (int valueFromSecondArray in array2)
+                {
+                    if (valueFromFirstArray == valueFromSecondArray)
+                    {
+                        Console.WriteLine((valueFromSecondArray));
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
